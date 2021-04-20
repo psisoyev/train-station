2af076c25222d63f2b20fbc0024f1fbfcef6b125
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-04-20 00:29:15 +0000

    Update cats-core to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,8 +39,8 @@ object Dependencies {
 }
 
 object Version {
-  val cats = "2.3.1"
-  val catsEffect = "2.3.1"
+  val cats = "2.6.0"
+  val catsEffect = "2.6.0"
   val zio = "1.0.6"
   val zioCats = "2.4.0.0"
   val slf4j = "1.7.30"
