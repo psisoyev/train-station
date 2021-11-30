127e90730d76f6e0a3276146f3fde1533d78edd3
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-11-30 00:18:37 +0000

    Update http4s-blaze-server, http4s-circe, ... to 1.0-303-5020468-SNAPSHOT

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Version {
   val zioCats        = "2.5.1.0"
   val slf4j          = "1.7.32"
   val fs2Core        = "2.4.2"
-  val http4s         = "1.0-234-d1a2b53"
+  val http4s         = "1.0-303-5020468-SNAPSHOT"
   val kindProjector  = "0.13.2"
   val ciris          = "1.2.1"
   val circe          = "0.14.1"
