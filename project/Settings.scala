aae02c4bf7c188a72035d0438c181761ef94eb12
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-01-03 16:54:26 +0100

    Remove scalafmt plugin

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,18 +1,18 @@
 import Dependencies._
 import com.typesafe.sbt.packager.Keys._
-import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
-import sbt.Keys.{scalacOptions, _}
+import sbt.Keys.{ scalacOptions, _ }
 import sbt._
 
 object Settings {
 
-  val commonSettings = {
+  val commonSettings =
     Seq(
       scalaVersion := "2.13.7",
       scalacOptions := Seq(
         "-Ymacro-annotations",
         "-deprecation",
-        "-encoding", "utf-8",
+        "-encoding",
+        "utf-8",
         "-explaintypes",
         "-feature",
         "-unchecked",
@@ -23,25 +23,19 @@ object Settings {
         "-Xfatal-warnings"
       ),
       version := (version in ThisBuild).value,
-      scalafmtOnCompile := true,
       testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework"),
-
       cancelable in Global := true,
       fork in Global := true, // https://github.com/sbt/sbt/issues/2274
       resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-
       mainClass in Compile := Some("com.psisoyev.train.station.Main"),
-
       addCompilerPlugin(contextApplied),
       addCompilerPlugin(kindProjector),
-
       dockerBaseImage := "openjdk:jre-alpine",
       dockerUpdateLatest := true
     )
-  }
 
   val serviceDependencies = List(cats, catsEffect, neutronCore, slf4j, zioCats) ++ zioTest
-  val routeDependencies = http4s
-  val serverDependencies = List(neutronCirce, ciris) ++ zio
-  val domainDependencies = List(newtype) ++ circe
+  val routeDependencies   = http4s
+  val serverDependencies  = List(neutronCirce, ciris) ++ zio
+  val domainDependencies  = List(newtype) ++ circe
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,3 @@
-addSbtPlugin("org.scalameta"             % "sbt-scalafmt"        % "2.4.6")
 addSbtPlugin("au.com.onegeek"            % "sbt-dotenv"          % "2.1.233")
 addSbtPlugin("com.github.sbt"            % "sbt-native-packager" % "1.9.7")
 addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat"        % "0.1.20")
