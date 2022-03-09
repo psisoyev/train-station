837dc8230ed4cb1d6c78001043f6ea6841ca53d5
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-03-09 00:25:14 +0000

    Update cats-effect to 3.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
 object Version {
   val zio            = "1.0.12"
   val cats           = "2.7.0"
-  val catsEffect     = "3.3.6"
+  val catsEffect     = "3.3.7"
   val zioCats        = "3.2.9.1"
   val slf4j          = "1.7.36"
   val fs2Core        = "3.2.3"
