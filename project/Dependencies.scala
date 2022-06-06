90bbb645b3ba8a0cb9b1538dbf839c6548b8f646
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-06-07 00:15:14 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.15 (#196)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -39,7 +39,7 @@ object Dependencies {
 }
 
 object Version {
-  val zio            = "1.0.14"
+  val zio            = "1.0.15"
   val cats           = "2.7.0"
   val catsEffect     = "3.3.12"
   val zioCats        = "3.2.9.1"
