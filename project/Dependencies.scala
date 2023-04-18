0bbdef5699820a9971a39335eea976c4c6051229
Pavels Sisojevs <p.sisoyev@gmail.com>
2023-04-18 00:28:31 +0000

    Update zio, zio-test, zio-test-sbt to 2.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
 }
 
 object Version {
-  val zio            = "2.0.10"
+  val zio            = "2.0.13"
   val cats           = "2.8.0"
   val catsEffect     = "3.4.8"
   val zioCats        = "23.0.0.4"
