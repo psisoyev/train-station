4992d6beea749ec787e81d81bdcd38e947379807
Pavels Sisojevs <p.sisoyev@gmail.com>
2023-06-29 00:37:16 +0000

    Update http4s-circe, http4s-dsl, ... to 0.23.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Version {
   val zioCats        = "23.0.03"
   val slf4j          = "2.0.7"
   val fs2Core        = "3.2.3"
-  val http4s         = "0.23.18"
+  val http4s         = "0.23.22"
   val kindProjector  = "0.13.2"
   val ciris          = "3.1.0"
   val circe          = "0.14.5"
