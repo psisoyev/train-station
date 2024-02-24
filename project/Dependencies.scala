b49a2b4de1377c78f6e0da0a2b09dfc6bd4b16dc
Pavels Sisojevs <p.sisoyev@gmail.com>
2024-02-24 00:03:15 +0000

    Update kind-projector to 0.13.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Version {
   val slf4j          = "2.0.12"
   val fs2Core        = "3.2.3"
   val http4s         = "0.23.18"
-  val kindProjector  = "0.13.2"
+  val kindProjector  = "0.13.3"
   val ciris          = "3.5.0"
   val circe          = "0.14.6"
   val newtype        = "0.4.4"
